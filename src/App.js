efd13c8cfe2a5a250927e29e8f3f4011fcd3878e
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-12-29 00:25:35 +0100

    Routing implemented in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Navbar from './main-layout/Navbar/Navbar';
+import ActionContainer from './main-layout/ActionContainer/ActionContainer';
+import Home from './pages/Home/Home';
+import PokemonCompare from './pages/PokemonCompare/PokemonCompare';
+import PokemonProfile from './pages/PokemonProfile/PokemonProfile';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Navbar />
+      <main>
+          <Router>
+              <Switch>
+                  <Route path='/' exact component={Home} />
+                  <Route path='/pokemon-compare' component={PokemonCompare} />
+                  <Route path='/pokemon/:id' component={PokemonProfile} />
+                  <Route component={NotFound} />
+              </Switch>
+          </Router>
+          <ActionContainer/>
+      </main>
+    </>
   );
 }
 
