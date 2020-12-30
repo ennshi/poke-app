5e05626c4b40ccf1447680366f462643d3d22903
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-12-30 23:10:15 +0100

    Default colors added

---
+++ src/constants/colors.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+    BLUE: '#2196f3',
+    RED: '#f44336',
+    PURPLE: '#421e71',
+    YELLOW: '#fad238',
+    ORANGE: '#f79c07',
+    GREEN: '#009b48',
+    PINK: '#cf658b'
+};
