8e270d3da5eaefbb516899518ee01cd3d08a6544
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-12-30 22:33:33 +0100

    Navbar styling developed

--- src/main-layout/Navbar/Navbar.js
+++ src/main-layout/Navbar/Navbar.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import AlbumIcon from '@material-ui/icons/Album';
+import useStyles from './styles';
+import Hidden from '@material-ui/core/Hidden';
 
 export default () => {
+    const classes = useStyles();
     return (
-        <nav>
-            <h1>Pok&eacute; App</h1>
-            <button>Show Mobile Actions</button>
-        </nav>
+        <>
+        <AppBar position="static" className={classes.navbar}>
+            <Toolbar className={classes.navbar__toolbar}>
+                <div className={classes.navbar__logo}>
+                    <AlbumIcon color="secondary" />
+                    <Typography variant="h6" color="secondary">
+                        <span className={classes['navbar__logo-text']}> Pok&eacute; App</span>
+                    </Typography>
+                </div>
+                <Hidden only={['lg']}>
+                    <IconButton edge="end" aria-label="actions">
+                        <ChevronLeftIcon color="primary"/>
+                    </IconButton>
+                </Hidden>
+            </Toolbar>
+        </AppBar>
+    </>
     )
 };
 
---
+++ src/main-layout/Navbar/styles.js
@@ -0,0 +1,47 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    navbar: {
+        backgroundColor: 'white',
+    },
+    navbar__toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    navbar__logo: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    'navbar__logo-text': {
+        color: 'white',
+        paddingLeft: '6px',
+        textShadow: logoTextShadow(theme.palette.primary.main)
+    }
+}));
+
+const logoTextShadow = (color) => (`1px 1px 0 ${color},
+            -1px 1px 0 ${color},
+            1px -1px 0 ${color},
+            -1px -1px 0 ${color},
+            0px 1px 0 ${color},
+            0px -1px 0 ${color},
+            -1px 0px 0 ${color},
+            1px 0px 0 ${color},
+            2px 2px 0 ${color},
+            -2px 2px 0 ${color},
+            2px -2px 0 ${color},
+            -2px -2px 0 ${color},
+            0px 2px 0 ${color},
+            0px -2px 0 ${color},
+            -2px 0px 0 ${color},
+            2px 0px 0 ${color},
+            1px 2px 0 ${color},
+            -1px 2px 0 ${color},
+            1px -2px 0 ${color},
+            -1px -2px 0 ${color},
+            2px 1px 0 ${color},
+            -2px 1px 0 ${color},
+            2px -1px 0 ${color},
+            -2px -1px 0 ${color};`
+);
