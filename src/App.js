68bd13a42a5bc8c126256395eefbab1da7d41f81
Anna Sh <47815524+ennshi@users.noreply.github.com>
2020-12-30 23:11:22 +0100

    Random background color set

--- src/App.js
+++ src/App.js
@@ -10,25 +10,27 @@ import NotFound from './pages/NotFound/NotFound';
 import './styles';
 import {mainTheme} from './styles';
 import {ThemeProvider} from '@material-ui/core/styles';
+import {useStyles} from './styles';
 
 
 function App() {
-  return (
-    <ThemeProvider theme={mainTheme}>
-      <Navbar />
-      <main>
-          <Router>
-              <Switch>
-                  <Route path='/' exact component={Home} />
-                  <Route path='/pokemon-compare' component={PokemonCompare} />
-                  <Route path='/pokemon/:id' component={PokemonProfile} />
-                  <Route component={NotFound} />
-              </Switch>
-          </Router>
-          <ActionContainer/>
-      </main>
-    </ThemeProvider>
-  );
+    const classes = useStyles();
+    return (
+        <ThemeProvider theme={mainTheme}>
+          <Navbar />
+          <main className={classes.app__main}>
+              <Router>
+                  <Switch>
+                      <Route path='/' exact component={Home} />
+                      <Route path='/pokemon-compare' component={PokemonCompare} />
+                      <Route path='/pokemon/:id' component={PokemonProfile} />
+                      <Route component={NotFound} />
+                  </Switch>
+              </Router>
+              <ActionContainer/>
+          </main>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,8 @@
 import {createMuiTheme} from '@material-ui/core/styles';
 import {red, blue, green} from '@material-ui/core/colors';
+import {makeStyles} from '@material-ui/core/styles';
+import {randomColor} from './utils/randomColor';
+import {COLORS} from './constants/colors';
 
 export const mainTheme = createMuiTheme({
     palette: {
@@ -12,5 +15,14 @@ export const mainTheme = createMuiTheme({
             'Righteous',
             'serif'
         ]
+    }
+});
+
+export const useStyles = makeStyles({
+    app__main: {
+        width: '100%',
+        marginTop: '-20px',
+        minHeight: '89.5vh',
+        backgroundColor: randomColor(Object.values(COLORS))
     }
 });
