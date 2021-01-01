3312be8fb2854dda549c5ddee4ca526790c89659
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-01 23:59:25 +0100

    Default colors updated

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,9 +1,12 @@
 export const COLORS = {
     BLUE: '#2196f3',
     RED: '#f44336',
-    PURPLE: '#421e71',
+    PURPLE: '#48426d',
     YELLOW: '#fad238',
     ORANGE: '#f79c07',
     GREEN: '#009b48',
-    PINK: '#cf658b'
+    PINK: '#cf658b',
+    BLACK: '#212121',
+    WHITE: '#fafafa',
+    GREY: '#607d8b'
 };
