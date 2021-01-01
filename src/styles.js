c3f947208e3f3e763b9ae70db71a4091f617a0ae
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-02 00:00:22 +0100

    fontSizes of headers set

--- src/styles.js
+++ src/styles.js
@@ -14,14 +14,22 @@ export const mainTheme = createMuiTheme({
         fontFamily: [
             'Righteous',
             'serif'
-        ]
+        ],
+        h4: {
+            fontSize: '1.6rem'
+        },
+        h5: {
+            fontSize: '1.4rem'
+        },
+        h6: {
+            fontSize: '1.2rem'
+        }
     }
 });
 
 export const useStyles = makeStyles({
     app__main: {
         width: '100%',
-        marginTop: '-20px',
         minHeight: '89.5vh',
         backgroundColor: randomColor(Object.values(COLORS))
     }
