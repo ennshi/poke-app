dfa61af5c808d78a7c955282cc2f3a7e9f782d01
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-03 16:45:01 +0100

    redux connected to Home

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default () => {
+export default ({pokemons}) => {
+    const pokemonList = pokemons.map((pokemon, i) => <p key={i}>{pokemon.name}</p>);
     return (
         <>
             <h1>Pokemon List</h1>
+            {pokemonList}
         </>
     )
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import HomeHeader from './HomeHeader';
 import PokemonList from '../../components/PokemonList/PokemonList';
+import {useDispatch, useSelector} from 'react-redux'
+import {requestAllPokemons, requestFilteredPokemons} from '../../redux/actions/request_pokemons';
 
 export default () => {
+    const dispatch = useDispatch();
+    const pokemons = useSelector(state => state.pokemons.pokemons);
+    const error = useSelector(state => state.pokemons.error);
+    const filter = useSelector(state => state.search.filter);
+    useEffect(() => {
+        dispatch(requestAllPokemons());
+    }, []);
+    useEffect(() => {
+        if(filter) {
+            dispatch(requestFilteredPokemons());
+        }
+    }, [filter]);
     return (
         <>
             <HomeHeader />
-            <PokemonList />
+            { !error && <PokemonList pokemons={pokemons}/> }
+            { error && <h1>{error}</h1> }
         </>
     )
 }
