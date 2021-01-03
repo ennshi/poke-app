824b49c3f2df4b52beb263e19063912a6106646d
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-03 19:26:19 +0100

    requestPokemonByName implemented in Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,21 +2,23 @@ import React, {useEffect} from 'react';
 import HomeHeader from './HomeHeader';
 import PokemonList from '../../components/PokemonList/PokemonList';
 import {useDispatch, useSelector} from 'react-redux'
-import {requestAllPokemons, requestFilteredPokemons} from '../../redux/actions/request_pokemons';
+import {requestAllPokemons, requestFilteredPokemons, requestPokemonByName} from '../../redux/actions/request_pokemons';
 
 export default () => {
     const dispatch = useDispatch();
     const pokemons = useSelector(state => state.pokemons.pokemons);
     const error = useSelector(state => state.pokemons.error);
     const filter = useSelector(state => state.search.filter);
-    useEffect(() => {
-        dispatch(requestAllPokemons());
-    }, []);
+    const searchName = useSelector(state => state.search.searchName);
     useEffect(() => {
         if(filter) {
             dispatch(requestFilteredPokemons());
+        } else if(searchName) {
+            dispatch(requestPokemonByName());
+        } else {
+            dispatch(requestAllPokemons());
         }
-    }, [filter]);
+    }, [filter, searchName]);
     return (
         <>
             <HomeHeader />
