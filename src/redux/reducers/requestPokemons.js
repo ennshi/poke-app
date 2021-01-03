b379475002b53c8edc864c4ed8c651c6d29eac10
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-03 16:29:16 +0100

    requestPokemons reducer updated, reset to initial state on new search

--- src/redux/reducers/requestPokemons.js
+++ src/redux/reducers/requestPokemons.js
@@ -1,4 +1,4 @@
-import {POKEMONS_REQUEST} from '../constants';
+import {POKEMONS_REQUEST, SEARCH_RESET, SET_FILTER, SET_SEARCH_NAME} from '../constants';
 
 const initialState = {
     page: 1,
@@ -8,7 +8,7 @@ const initialState = {
     loading: false
 };
 
-export default (state = initialState, {type, payload}) => {
+export default (state = {...initialState}, {type, payload}) => {
     switch (type) {
         case POKEMONS_REQUEST.PENDING:
             return Object.assign(state, {
@@ -27,6 +27,13 @@ export default (state = initialState, {type, payload}) => {
                 initialState,
                 { error: 'Something went wrong' }
                 );
+        case SET_FILTER:
+        case SEARCH_RESET:
+        case SET_SEARCH_NAME:
+            return Object.assign(
+                state,
+                initialState
+            );
         default:
             return state;
     }
