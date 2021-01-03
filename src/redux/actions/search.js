757f21a2062de82a92000319fb4b914c6b32f069
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-03 19:23:00 +0100

    addSearchName action created

--- src/redux/actions/search.js
+++ src/redux/actions/search.js
@@ -1,8 +1,15 @@
-import {SET_FILTER} from '../constants';
+import {SET_FILTER, SET_SEARCH_NAME} from '../constants';
 
 export const addSearchFilter = ({property, value} = {}) => {
     return ({
         type: SET_FILTER,
         payload: {property, value}
+    });
+};
+
+export const addSearchName = (name) => {
+    return ({
+        type: SET_SEARCH_NAME,
+        payload: name
     });
 };
