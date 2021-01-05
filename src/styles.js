192cc0355b9d6a3c048b65b1053d56d7da465844
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-05 22:54:24 +0100

    bgColor deleted from styles

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,6 @@
 import {unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
 import {red, blue, green} from '@material-ui/core/colors';
 import {makeStyles} from '@material-ui/core/styles';
-import {randomColor} from './utils/randomColor';
-import {COLORS} from './constants/colors';
 
 export const mainTheme = createMuiTheme({
     palette: {
@@ -34,7 +32,6 @@ export const useStyles = makeStyles((theme) => ({
     app__main: {
         width: '100%',
         minHeight: '89.5vh',
-        backgroundColor: randomColor(Object.values(COLORS))
     },
     app__content: {
         width: '90%',
