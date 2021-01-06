348494e188f5cbf1372653a03539c80736720aec
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-07 00:33:02 +0100

    setTextColor util created

--- src/utils/randomColor.js
+++ src/utils/randomColor.js
@@ -3,3 +3,8 @@ export const randomColor = (colors) => (colors[Math.floor(Math.random()*(colors.
 export const setBgColor = (filter, colors) => {
     return filter ? filter.color : randomColor(colors);
 };
+
+export const setTextColor = (labelColor, colorList, baseTextColor, lighterTextColor) => {
+    const colorArr = [colorList.GREY, colorList.PURPLE, colorList.BLACK, colorList.GREEN];
+    return Object.values(colorArr).includes(labelColor) ? lighterTextColor : baseTextColor;
+};
