5d02f4414cb382867c32a3080e1f0484e3e120e1
Anna Sh <47815524+ennshi@users.noreply.github.com>
2021-01-07 00:32:30 +0100

    HomeHeader updated, ChipFilter applied

--- src/pages/Home/HomeHeader.js
+++ src/pages/Home/HomeHeader.js
@@ -1,54 +1,35 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 import Chip from '@material-ui/core/Chip';
 import {COLORS} from '../../constants/colors';
-import {COLOR_CHIPS, TYPE_CHIPS} from '../../constants/chips';
 import {useDispatch} from 'react-redux';
-import {resetSearch} from '../../redux/actions/search';
 import {useStyles} from './styles';
 import AlbumIcon from '@material-ui/icons/Album';
 import {Box} from '@material-ui/core';
+import FilterChip from '../../components/FilterChip/FilterChip';
 
 export default ({filter, searchName}) => {
     const classes = useStyles();
     const dispatch = useDispatch();
-    let chipData = {
-        icon: true,
-        label: '',
-        backgroundColor: COLORS.WHITE,
-        reset: false
-    };
-    if(searchName) {
-        chipData = {
-            backgroundColor: COLORS.RED,
-            label: searchName,
-            reset: true
-        };
-    }
-    if(filter) {
-        const bgColor = filter.property === 'color' ?
-            COLOR_CHIPS.find(ch => ch.label === filter.value).color :
-            TYPE_CHIPS.find(ch => ch.label === filter.value).color;
-        chipData = {
-            label: filter.value,
-            backgroundColor: bgColor,
-            reset: true
-        }
-    }
     return (
         <Box className={classes['home-header__container']} boxShadow={1}>
-            <Chip
-                icon={chipData.icon && <AlbumIcon className={classes['home-header__icon']}/>}
-                label={`${chipData.label}`}
-                clickable={chipData.reset}
-                style={{backgroundColor: `${chipData.backgroundColor}`}}
-                onDelete={chipData.reset ?
-                    () => (dispatch(resetSearch())) :
-                    null
-                }
-                className={classes['home-header__chip']}
-            />
+            {(filter || searchName) ?
+                <FilterChip
+                    chip={{
+                        color: filter ? filter.color : COLORS.RED,
+                        label: filter ? filter.value : searchName
+                    }}
+                    type=""
+                    dispatch={dispatch}
+                    filter={filter}
+                    searchName={searchName}
+                    className={classes['home-header__chip']}
+                /> :
+                <Chip
+                    icon={<AlbumIcon className={classes['home-header__icon']}/>}
+                    style={{backgroundColor: 'white'}}
+                />
+            }
             <Typography variant="h1" color="primary">Pok&eacute;mons</Typography>
         </Box>
     )
